b1988cc2a3a1dbe2e3a95cb5f36d74edef503f8d
efoejean <efoejean@gmail.com>
2022-09-14 21:49:35 -0500

    add services to the modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsService } from './clients.service';
+import { PrismaService } from './prisma.service';
+import { StylistsService } from './stylists.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [StylistsService, ClientsService, PrismaService],
 })
 export class AppModule {}
