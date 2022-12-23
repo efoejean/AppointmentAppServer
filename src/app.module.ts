ba6e10c978f595953470f21e9c7ae696f799c256
efoejean <efoejean@gmail.com>
2022-12-22 19:46:39 -0600

    adding appointment service to the module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppointmentsService } from './appointments.service';
 import { ClientsService } from './clients.service';
 import { PrismaService } from './prisma.service';
 import { StylistsService } from './stylists.service';
@@ -7,6 +8,11 @@ import { StylistsService } from './stylists.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [StylistsService, ClientsService, PrismaService],
+  providers: [
+    StylistsService,
+    ClientsService,
+    PrismaService,
+    AppointmentsService,
+  ],
 })
 export class AppModule {}
